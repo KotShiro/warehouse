f57647a1a9bd1456711048b119cf8ecff8fb549b
Никита А. Ковалевич <C:\Users\kovalevich\AppData\Roaming\The Bat!>
2021-02-23 16:59:25 +0300

    init: this app

---
+++ js-camera/main.js
@@ -0,0 +1,72 @@
+(function () {
+    let width = 320
+    let height = 0
+
+    let streaming = false
+
+    let video = null
+    let canvas = null
+    let photo = null
+    let statbutton = null
+
+    function startup() {
+        video = document.getElementById('video')
+        canvas = document.getElementById('canvas')
+        photo = document.getElementById('photo')
+        statbutton = document.getElementById('statbutton')
+
+        navigation.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(function(stream){
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+
+        video.addEventListener('canplay', function(ev){
+            if (!streaming) {
+            height = video.videoHeight / (video.videoWidth/width);
+
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+            }
+        }, false);
+
+        startbutton.addEventListener('click', function(ev){
+            takepicture();
+            ev.preventDefault();
+        }, false);
+
+        clearphoto();
+    }
+
+    function clearphoto() {
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+      }
+
+      function takepicture() {
+        var context = canvas.getContext('2d');
+        if (width && height) {
+          canvas.width = width;
+          canvas.height = height;
+          context.drawImage(video, 0, 0, width, height);
+
+          var data = canvas.toDataURL('image/png');
+          photo.setAttribute('src', data);
+        } else {
+          clearphoto();
+        }
+      }
+
+      window.addEventListener('load', startup, false);
+
+})();
