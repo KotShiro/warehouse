630a8d33787995f6fc86ececb0d6cb1ddebe21a4
Никита А. Ковалевич <C:\Users\kovalevich\AppData\Roaming\The Bat!>
2021-02-25 10:37:46 +0300

    refactor: script js - HTML el-s, fn callAnswer

--- js-callapp/main.js
+++ js-callapp/main.js
@@ -5,7 +5,9 @@
         const btnCallToNode = document.querySelector('.btnCallToNode')
         const otherPeerId = document.getElementById('otherPeerId')
         btnCallToNode.addEventListener('click', ()=>{callToNode(otherPeerId.value)})
-
+        const callinfo = document.getElementById('callinfo')
+        const remVideo = document.getElementById('remVideo')
+        const myVideo = document.getElementById('myVideo')
 
         const peer = new Peer()
 
@@ -13,50 +15,67 @@
 			document.getElementById('myid').innerHTML=peerID;
 		})
 
+        function creatElement(teg, className = '', content = '') {
+            const element = document.createElement(teg)
+            className ? element.classList.add(className) : className
+            element.innerText = content
+            return element
+        }
+
         peer.on('call', function(call) {
-            // Answer the call, providing our mediaStream
               peercall=call;
-              document.getElementById('callinfo').innerHTML="Входящий звонок <button onclick='callanswer()' >Принять</button><button onclick='callcancel()' >Отклонить</button>";
+              const btnAnswer = creatElement('button', 'callanswer', 'Принять')
+              const btnCancel = creatElement('button', 'callcancel', 'Отклонить')
+              callinfo.appendChild(creatElement('p', '', 'Входящий звонок'))
+              callinfo.appendChild(btnAnswer)
+              callinfo.appendChild(btnCancel)
+              btnAnswer.addEventListener('click', ()=>{callanswer()})
+              btnCancel.addEventListener('click', ()=>{callcancel()})
         })
 
         function callanswer() {
             navigator.mediaDevices.getUserMedia ({ audio: true, video: true }).then(function(mediaStream) {
-                var video = document.getElementById('myVideo');
                 peercall.answer(mediaStream); // отвечаем на звонок и передаем свой медиапоток собеседнику
                 //peercall.on ('close', onCallClose); //можно обработать закрытие-обрыв звонка
-                video.srcObject = mediaStream; //помещаем собственный медиапоток в объект видео (чтоб видеть себя)
-                document.getElementById('callinfo').innerHTML="Звонок начат... <button onclick='callclose()' >Завершить звонок</button>"; //информируем, что звонок начат, и выводим кнопку Завершить
-                video.onloadedmetadata = function(e) {//запускаем воспроизведение, когда объект загружен
-                    video.play();
+                myVideo.srcObject = mediaStream; //помещаем собственный медиапоток в объект видео (чтоб видеть себя)
+                const btnCancel = creatElement('button', 'callcancel', 'Завершить звонок')
+                callinfo.innerHTML = ''
+                callinfo.appendChild(creatElement('p', '', 'Звонок начат...'))
+                callinfo.appendChild(btnCancel)
+                myVideo.onloadedmetadata = function(e) {//запускаем воспроизведение, когда объект загружен
+                    myVideo.play();
                 };
                 setTimeout(function() {
-                            //входящий стрим помещаем в объект видео для отображения
-                        document.getElementById('remVideo').srcObject = peercall.remoteStream;
-                    document.getElementById('remVideo').onloadedmetadata= function(e) {
-            // и запускаем воспроизведение когда объект загружен
-                            document.getElementById('remVideo').play();
+                    //входящий стрим помещаем в объект видео для отображения
+                    remVideo.srcObject = peercall.remoteStream;
+                    remVideo.onloadedmetadata= function(e) {
+                    // и запускаем воспроизведение когда объект загружен
+                    remVideo.play();
+                    btnCancel.addEventListener('click', ()=>{callcancel()})
                     };
                 },1500);
 
             }).catch(function(err) { console.log(err.name + ": " + err.message); });
         }
 
+        function callcancel() {
+        }
+
         function callToNode(peerId) { //вызов
                 navigator.mediaDevices.getUserMedia ({ audio: true, video: true }).then(function(mediaStream) {
-                var video = document.getElementById('myVideo');
                 peercall = peer.call(peerId,mediaStream); //звоним, указав peerId-партнера и передав свой mediaStream
                 peercall.on('stream', function (stream) { //нам ответили, получим стрим
                     setTimeout(function() {
-                    document.getElementById('remVideo').srcObject = peercall.remoteStream;
-                    document.getElementById('remVideo').onloadedmetadata= function(e) {
-                    document.getElementById('remVideo').play();
+                    remVideo.srcObject = peercall.remoteStream;
+                    remVideo.onloadedmetadata= function(e) {
+                    remVideo.play();
                     };
                     },1500);
             });
             //  peercall.on('close', onCallClose);
-            video.srcObject = mediaStream;
-            video.onloadedmetadata = function(e) {
-                video.play();
+            myVideo.srcObject = mediaStream;
+            myVideo.onloadedmetadata = function(e) {
+                myVideo.play();
             };
             }).catch(function(err) { console.log(err.name + ": " + err.message); });
         }
